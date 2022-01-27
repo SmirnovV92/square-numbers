3232afcf8cf56d1f08f5e96fa63a485523d6a044
Lorenzo <smirnov_bvb@mail.ru>
2022-01-27 21:41:13 +0300

    Created new tests

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,12 +3,11 @@ package ru.netology.sqr;
 public class SQRService {
     public long enumeration(long minLimit, long maxLimit) {
         int counter = 0;
-        for (int i = 10; i >= 10 && i <= 99; i++) {
-            if (Math.pow(i, 2) >= minLimit && Math.pow(i, 2) <= maxLimit)
+        for (int i = 10; i <= 99; i++) {
+            if (Math.pow(i, 2) >= minLimit && Math.pow(i, 2) <= maxLimit) {
                 counter = counter + 1;
+            }
         }
-        {
-            return counter;
-        }
+        return counter;
     }
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -14,4 +14,34 @@ class SQRServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void minShouldEnumeration() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 0;
+        long actual = sqrService.enumeration(0, 99);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxShouldEnumeration() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 2;
+        long actual = sqrService.enumeration(9500, 10000);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void allShouldEnumeration() {
+        SQRService sqrService = new SQRService();
+
+        int expected = 90;
+        long actual = sqrService.enumeration(100, 9801);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
